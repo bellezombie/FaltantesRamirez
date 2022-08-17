9817cdb8ea7360afc07b36001224331d0c06516b
Belle <51927520+bellezombie@users.noreply.github.com>
2022-08-16 22:14:32 -0300

    agregando cosas de la clase

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function Checkout() {
+  const [name,setName] = useState ('');
+  const [cel,setCel] = useState ('');
+  const [email,setEmail] = useState ('');
+
+
   return (
-    <div>Checkout</div>
+    <div>
+      <input type={'text'} placeholder={"Nombre"} value={name} onChange={(e) => setName(e.target.value)} />
+      <input type={'cel'} placeholder={"Celular"} value={cel} onChange={(e) => setCel(e.target.value)} />
+      <input type={'email'} placeholder={"Email"} value={email} onChange={(e) => setEmail(e.target.value)} />
+      <button>Terminar compra</button>
+    </div>
   )
 }
